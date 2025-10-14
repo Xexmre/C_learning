1f6ca6f172b5db4c0fd0656731d15c2777df8951
Xe_xmre <Xe_xmre@outlook.com>
2025-10-14 15:22:29 +0800

    修改了一些文件

---
+++ c1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define student_nunber 20252995
+
+int main(void)
+{
+    const char frames[] = "|/-\\";
+    printf("大家好，我是来自数理学院的黄宇轩，我的学号是%d，我喜欢研究计算机技术", student_nunber);
+    return 0;
+}
--- test_1.c
+++ test_1.c
@@ -3,4 +3,5 @@
 int main(void)
 {
     printf("hello");
+    return 0;
 }
