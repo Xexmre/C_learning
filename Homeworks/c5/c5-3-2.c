c3eda9dab09b01cb7949336c7a446653e48ed5af
Xe_xmre <Xe_xmre@outlook.com>
2025-11-20 22:17:56 +0800

    c5-edit

--- Homeworks/c5/c5-3-2.c
+++ Homeworks/c5/c5-3-2.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define IN 100.01
-#define N 10
 int main()
 {
     float in;         // 输入的总金额
@@ -11,12 +9,14 @@ int main()
     int w_sum = 0;    // 总权重
     int test_sum = 0; // 测试总金额
     int diff;         // 金额修正值
+    FILE *out;
 
     srand((unsigned)time(NULL));
+    out = fopen("red_packet.txt", "w");
 
     // -------------- 输入部分 --------------
-    // scanf("%f %d", &in, &n);
-    in = IN, n = N;          // for testing
+    printf("请输入总金额和份数（以空格分隔）：");
+    scanf("%f %d", &in, &n);
     m_sum = (int)(in * 100); // 修正有效总金额
     printf("总金额 = %.2f\n", (float)(m_sum) / 100);
 
@@ -37,9 +37,13 @@ int main()
     diff = m_sum - test_sum; // 计算金额修正值
     for (int i = 0; i < diff; i++)
     {
-        m[rand()%(n-1)]++; // 将修正值分配到前diff个部分
+        m[rand() % (n - 1)]++; // 将修正值分配到前diff个部分
+        test_sum++;
     }
-
-    printf("test_sum = %d\nreal_sum = %d", test_sum, m_sum); // 测试总金额是否相等
+    // -------------- 输出部分 --------------
+    for (int i = 0; i < n; i++)
+        fprintf(out, "第 %d 份金额 = %.2f\n", i + 1, (float)(m[i]) / 100);
+    printf("test_sum = %d\nreal_sum = %d\n", test_sum, m_sum); // 测试总金额是否相等
+    printf(test_sum == m_sum ? "金额分配正确！\n" : "金额分配错误！\n");
     return 0;
 }
