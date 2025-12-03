805d86224e47052054816a3627c6f1f7d515fdeb
Xe_xmre <Xe_xmre@outlook.com>
2025-12-03 12:09:47 +0800

    test-edit

--- Lab/chicken_problem.c
+++ Lab/chicken_problem.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-#define m_cost 5
-#define f_cost 3
-#define c_cost 1.0 / 3.0
 int main(void)
 {
     int chicken = 100, m = 0, f = 0;
     double money = 100;
     // 4f + 7m = 3/2 money (money - c_cost * chicken)
-    printf("4f + 7m = %.2lf", 3.0 / 2.0 * (money - c_cost * chicken));
-    for (m = 0; m <= 100.0 / 7; m++)
+    for (f = 0; f <= (3.0 / 2.0 * (money - 1.0 / 3.0 * chicken) / 4); f++)
     {
-        
+        if ((100 - 4 * f) % 7 == 0)
+        {
+            m = (100 - 4 * f) / 7;
+            printf("公鸡 = %d, 母鸡 = %d, 小鸡= %d\n", m, f, 100 - m - f);
+        }
     }
     return 0;
 }
