5c18bccf43b97c5e9d3f118b9e8e2e5934c555fa
Xe_xmre <Xe_xmre@outlook.com>
2025-12-03 12:01:08 +0800

    c6-fix

--- Homeworks/c6/c6-2-8.c
+++ Homeworks/c6/c6-2-8.c
@@ -2,12 +2,12 @@
 #define MAX 20
 int main()
 {
-    int a, b = 1, c = 1;
+    int a, b = 1, c = 2;
     double sum = 0.0;
     for (int i = 1; i <= MAX; ++i)
     {
         sum += (double)(b) / c;
-        printf("%2i : %d / %d\n", i, b, c);
+        printf("%2i : %d / %d\n", i, c, b);
         a = b, b = c, c = a + b;
     }
     printf("sum=%lf\n", sum);
